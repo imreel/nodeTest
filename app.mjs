a5d17171e6c685c919ab708d13567c2679e29461
Imre Lind <imreel@uia.no>
2024-01-17 16:56:51 +0100

    rettelse på kode

--- app.mjs
+++ app.mjs
@@ -5,8 +5,8 @@ const log = console.log;
 log(chalk.magenta('To') + chalk.greenBright(' atomer') + chalk.cyanBright(' møttes') 
 + chalk.blue(' og') + chalk.magenta(' den') + chalk.redBright(' ene') 
 + chalk.green(' sa:')
- + chalk.blueBright(' "Jeg') + chalk.yellowBright(' har') + chalk.grey(' mistet')
-+ chalk.bgCyan('et') + chalk.bgAnsi256(' elektorn"')
++ chalk.blueBright(' Jeg') + chalk.yellowBright(' har') + chalk.grey(' mistet')
++ chalk.bgCyan(' et') + chalk.bgMagenta(' elektorn')
 + chalk.redBright(' Den') + chalk.bgBlue(' andre') + chalk.blue(' spurte:')
 + chalk.yellow(' Er') + chalk.blackBright(' du') + chalk.cyanBright(' positiv?')
 
